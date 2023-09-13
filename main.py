262ed3ac1801116c5ec926df4a1996d013abb93f
Jaylen-L <jaylenlosh@ou.edu>
2023-09-12 19:46:46 -0500

    Second Commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 10, -4, 0]
+numbers = [1, 2, 3, 4]
 
 def sumArray(array):
     result = 0
@@ -8,3 +8,14 @@ def sumArray(array):
 
 print("The sum is:")
 print(sumArray(numbers))
+
+def productArray(array):
+    result = 0
+    for x in array:
+        if result == 0:
+            result = x
+        result *= x
+    return result
+
+print("The product is:")
+print(productArray(numbers))
